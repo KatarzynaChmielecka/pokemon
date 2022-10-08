5c7a232a99f6b09b2c25a31235f68fa44f5aef81
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-09 01:30:41 +0200

    Styled home page, changes in dimensions of pokemon card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,28 +2,20 @@ import ScrollToTop from 'react-scroll-to-top';
 import { useContext } from 'react';
 
 import AllPokemons from '../components/AllPokemons';
+import classes from './HomePage.module.css';
 import { PokemonContext } from '../context/PokemonContext';
 
 const HomePage = () => {
   const { inputText, isLoading } = useContext(PokemonContext);
 
   return (
-    <div
-    // style={{
-    //   display: 'flex',
-    //   flexWrap: 'wrap',
-
-    //   width: '100%',
-    //   height: '100vh',
-
-    // }}
-    >
+    <div className={classes['home-page-wrapper']}>
       {isLoading ? (
         <h1>Gathering pokemons...</h1>
       ) : (
         <AllPokemons searchInput={inputText} />
       )}
-      <ScrollToTop smooth color="white" style={{ background: 'black' }} />
+      <ScrollToTop smooth color="#fff" style={{ background: '#000' }} />
     </div>
   );
 };
