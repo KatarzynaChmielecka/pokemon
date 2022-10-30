a9d42fa8e39bc056e38afbe1609a43ef4d1385f7
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-30 16:01:17 +0100

    Added typescript to PokemonContext

---
+++ src/context/PokemonContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, useEffect, useState } from 'react';
+
+interface PokemonInterface {
+  name: string;
+  url: string;
+}
+
+export const PokemonContext = createContext<object | null>(null);
+export const PokemonContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [allPokemons, setAllPokemons] = useState<PokemonInterface[]>([]);
+  const [inputText, setInputText] = useState('');
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=2000';
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getAllPokemons = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        const getPokemon = (result: PokemonInterface[]) => {
+          result.map(async (pokemon) => {
+            const res = await fetch(
+              `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+            );
+            const resJson = await res.json();
+
+            setAllPokemons((currentList) => [...currentList, resJson]);
+
+            setIsLoading(false);
+          });
+        };
+        getPokemon(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getAllPokemons();
+  }, []);
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        allPokemons,
+        setAllPokemons,
+        inputText,
+        setInputText,
+        setIsLoading,
+        isLoading,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+};
